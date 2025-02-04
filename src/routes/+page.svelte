57a0140ef4be10366e47b6fd40794e70f186051f
Tonmoy Kumar <pramaniktonmoy2@gmail.com>
2025-02-04 12:41:41 +0600

    cursor pointer for explore button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
       <div class="mt-10 flex items-center justify-center gap-x-6">
         <button
           on:click={goToPage}
-          class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs cursor-pointer hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >Explore Movies <span aria-hidden="true">→</span></button
         >
       </div>
